2cc4d29597446040f4bfb9aed4548e1fab500c23
Constantin A <10349490+C1710@users.noreply.github.com>
2019-05-11 18:56:11 +0200

    Add a bazillion new skin color sequences and updated the skincolor scripts

--- skintone/emoji.py
+++ skintone/emoji.py
@@ -8,6 +8,8 @@ class Emoji:
     # This is the case for gender symbols which should be represented as emojis and not as characters
     fe0f_chars = {'♀', '♂'}
 
+    color_regex = re.compile("#([a-f0-9]{6,7})|rgb\(([0-9]{1,3},[0-9]{1,3},[0-9]{1,3})\)", re.IGNORECASE)
+
     def __init__(self, modifiers: dict, path: str, base: str, end: bool = False):
         """
         Create a new Emoji
@@ -45,7 +47,7 @@ class Emoji:
             # You don't need to convert from base to base
             if modifier != self.base:
                 self.new_modified_file(modifier)
-                print('{} auf Emoji {} angewendet.'.format(name, self.name))
+                print('{} applied to Emoji {}.'.format(name, self.name))
 
     def new_modified_file(self, modifier: Modifier):
         """
@@ -73,14 +75,13 @@ class Emoji:
         :param modifier: The Modifier which has to be applied
         :return: The altered SVG file's content
         """
-        # We're going to work on a copy of the content
         content = self.content
-        # All colors are indexed by their name. We'll replace one by one
-        for old, new in modifier.replace(self.base).items():
-            # Create the regular expression which will be used
-            old_regex = re.compile(old, re.IGNORECASE)
-            # ...Apply it
-            content = old_regex.sub(new, content)
+        colors = self.get_colors()
+        modifications = modifier.replace(colors, self.base)
+
+        # All colors to be replaced are indexed by their original string. We'll replace one by one
+        for old, new in modifications.items():
+            content = content.replace(old, new)
         return content
 
     def save(self, content: str, extension: str) -> None:
@@ -114,3 +115,35 @@ class Emoji:
         basename = '_'.join(base_seq)
         # Stitch it together and return the whole file path
         return os.path.join(directory, basename) + fileextension
+        
+    def get_colors(self) -> dict:
+        """
+        Extracts all color values and their corresponding strings
+        :return: A dict containing actually used color strings as keys and their RGBA values as values (i.e. '#ff0000': (255,0,0,0))
+        """
+        colors = list()
+        results = Emoji.color_regex.finditer(self.content)
+
+        for result in results:
+            index = result.group(0)
+            if result.group(1):
+                # Hexadecimal
+                value = result.group(1)
+                value = [value[0:2], value[2:4], value[4:6], value[6:8]]
+                # Add an alpha value if necessary
+                if not value[3]:
+                    value[3] = 'ff'
+                value = tuple(int(v, 16) for v in value)
+            elif result.group(2):
+                # RGB notation
+                value = result.group(2)
+                value = value.split(',')
+                value = tuple(map(int, value))
+            else:
+                # This cannot happen since it has either to be a hex string or in rgb notation
+                raise ValueError()
+            colors.append((index, value))
+
+        return dict(colors)
+
+            
