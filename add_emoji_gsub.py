73f559c94be70d367c68645428bcab5ee6efe100
guidotheelen <guido@kabisa.nl>
2020-04-14 09:28:11 +0200

    import exceptions

--- add_emoji_gsub.py
+++ add_emoji_gsub.py
@@ -18,14 +18,22 @@
 
 __author__ = "roozbeh@google.com (Roozbeh Pournader)"
 
-
 import sys
 
-from fontTools import agl
-from fontTools import ttLib
-from fontTools.ttLib.tables import otTables
+try:
+    from fontTools import agl
+except ImportError as er:
+    sys.exit("""Failed to import fontTools.
+             Please make sure 'pip install -r requirements.txt' runs succesfull""")
 
-from nototools import font_data
+from fontTools.ttLib.tables import otTables
+from fontTools import ttLib
+
+try:
+    from nototools import font_data
+except ImportError as er:
+    sys.exit("""Failed to import nototools.
+             Please make sure 'pip install -r requirements.txt' runs succesfull""")
 
 
 def create_script_list(script_tag='DFLT'):
