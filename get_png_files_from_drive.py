c116ae0fe9a2765ddf33d1afe0a8c04c87eb7047
guidotheelen <guido@kabisa.nl>
2020-04-17 08:41:11 +0200

    remove author

--- get_png_files_from_drive.py
+++ get_png_files_from_drive.py
@@ -12,9 +12,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Google Author(s): Guido Theelen
+# limitations under the License
 
 from __future__ import print_function
 import pickle
